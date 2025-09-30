a27b6849a51041b8a98622d15dd7b2bd9f3fe119
Artem Savchenko <valter.199669@gmail.com>
2025-09-30 19:03:30 +0300

    SQLite database connected

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Learning Shumeyko's FastAPI [![Watch video](https://img.youtube.com/vi/gBfkX9H3szQ/maxresdefault.jpg)](https://youtu.be/gBfkX9H3szQ?si=qEESCqo84Aco3GDB)
 
 Command for start project: uvicorn main:app --reload
-The only path: http://127.0.0.1:8000/home
+
+The only path: http://127.0.0.1:8000/docs and post method http://127.0.0.1:8000/tasks which you can use through swagger in the first path
--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
 
-app = FastAPI()
+from contextlib import asynccontextmanager
 
-@app.get('/home')
-def get_home():
-    return {'data': 'Hello world!'}
+from database import create_tables, delete_tables
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print('Base is cleaned')
+    await create_tables()
+    print('Base is ready')
+    yield
+    print('Shutdown')
+
+app = FastAPI(lifespan=lifespan)
+
+class STaskAdd(BaseModel):
+    name: str
+    description: str | None
+
+class Stask(STaskAdd):
+    id: int
+
+tasks = []
+
+@app.post('/tasks')
+async def add_task(
+    task: Annotated[STaskAdd, Depends()],
+):
+    tasks.append(task)
+    return {'ok': True}
+
+
+# @app.get('/tasks')
+# def get_tasks():
+#     task = Task(name='Запиши это видео')
+#     return {'data': task}
